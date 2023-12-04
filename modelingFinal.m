b7e380b8e99161cc584f1ebdf2576e2027cbbe3a
atomko6 <152230204+atomko6@users.noreply.github.com>
2023-12-04 13:09:11 -0500

    Add files via upload

--- modelingFinal.m
+++ modelingFinal.m
@@ -1,55 +1,39 @@
-%Set values
-dx = 1
-x = 0:dx:50;
+x = 0:1:50;
 B = 50
-dt = 150
-t = 0:dt:54000;
+t = 0:150:54000;
 g = 9.8
 S0 = .0002
 n = .01
 V = 4
 C =1
-%Building
-% beta = zeros(length(t),length(x));
-% alpha = zeros(length(t),length(x));
-Q = zeros(length(t),length(x));
 
-% Estimated Values
-Q0 = 510
-amp = 1.5
+beta = zeros(length(time),length(x));
+alpha = zeros(length(time),length(x));
+Q = zeros(length(time),length(x));
+%random once again bc we cant find one
+Q0 = 2
+%random values for amplitude and period lol
+amp = 3
 T = 10
-%average depth of the nile river is 8-11m
-y = 9.5
-% assuming river is a rectangle for cross sectional area
-A = B * y
-%Setting initial 
-alpha = 5
-beta = 15
 Q(:,1) = Q0*(1+ amp*sin((2*pi*t)/T))'
 
 
-for j = 2:length(t)-1
-    for i = 2:length(x)-1
+for i = 2:length(time)
+    for j = 2:length(x)-1
+   
+    A =  B * x(j);
     %R is guestimate lol for perimeter
-    % R = A/58;
-    % Sf = sqrt((V*n)/R^(2/3));
-    alpha = 2*(Q(j,i)/A(j,i))+(((g*A(j,i)/B)-(Q(j,i)^2/A(i,j)^2))/((Q(j,i)/A(j,i))*((5/3)-(4*R/3*B))));
-    beta = g*A*(Sf-S0);
-
-    Q(j+1,i) = Q(j,i) + alpha*(dt/dx)*(Q(j,i)-Q(j,i-1)) - beta*dt;
-
-
-    %Q(j,i+1) = Q(j,i) + alpha*Q(j+1,i)*(dt/dx) + beta;
-    % alpha(i,j) = 2*Q(i,j)/A +((((g*A)/B)-Q(i,j).^2/A.^2))/((Q(i,j)/A)* (5/3-(4.*R)/3.*B));
-    % beta(i,j) = g*A*(Sf-S0);
+    R = A/58;
+    Sf = sqrt((V*n)/R^(2/3));
+    alpha(i,j) = 2*Q(i,j)/A +((((g*A)/B)-Q(i,j).^2/A.^2))/((Q(i,j)/A)* (5/3-(4.*R)/3.*B));
+    beta(i,j) = g*A*(Sf-S0);
 
     % Q(i,j+1) = Q(i,j) - C*(Q(i,j)-Q(i,j-1));
     % Q(i,j+1) = Q(i,j)-alpha(i,j)*(150/1)*(Q(i,j)-Q(i-1,j))-beta(i,j)*150;
     end
-    %Q(1,i+1) = Q(2,i+1);
+    Q(1,j+1) = Q(2,j+1);
 end
-% figure
-plot(t,Q)
+
     % alphaM = (alpha(i+1,j)+alpha(i,j+1))/2
     % betaM =(beta(i+1,j)+beta(i,j+1))/2
     %
